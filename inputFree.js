c5343e088dd2f3285d8cfcbc603a5f491fdef576
Shima <shima.maleki@gmail.com>
2019-07-30 07:06:02 -0400

    0101IN

--- inputFree.js
+++ inputFree.js
@@ -1,10 +1,10 @@
 function clickCheck() {
-    if (document.getElementById('yesCheck').checked) {
+    if (document.getElementById('compareToURL').checked) {
         document.getElementById('ifYes').style.display = 'block';
         document.getElementById('ifClicked').style.display = 'none';
     }
-    else if(document.getElementById('clCheck').checked){
-        document.getElementById('ifClicked').style.display = 'block';
+    else if(document.getElementById('compareToGoogle').checked){
+        document.getElementById('googleResults').style.display = 'block';
         document.getElementById('ifYes').style.display = 'none';
     }
 
